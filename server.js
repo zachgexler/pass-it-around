6df11f6c4ed0c77885d8bb55bba38158bcf3a1c9
Zachary Gexler <zacharygexler@Zacharys-MacBook-Pro.local>
2022-08-30 08:30:29 -0400

    Pass It Around  express application created.

---
+++ server.js
@@ -0,0 +1,36 @@
+// require packages/dependacies
+
+require("express");
+
+console.log(require("express"))
+
+const express = require("express");
+
+// initialize the express application
+
+const app = express();
+
+//define routes
+// 99 Bottles of Redbull
+//http://localhost:3000
+// I had help from internet and friends. It works, but I don't understand the jQuery, or how it's working or even where it's pulling from.
+
+app.get("/", (req, res) => {
+    res.send(`<h3>99 Bottles of redbull on the wall</h3>
+      <a href ="/98">Take one down, pass it around</a>`);
+  });
+  
+  app.get("/:numOfBottles", (req, res) => {
+    let bottleNumbers = req.params.numOfBottles;
+    if (bottleNumbers > 0) {
+      res.send(`<h3>${bottleNumbers} Bottles of redbull on the wall</h3>
+          <a href ="/${bottleNumbers - 1}">Take one down, pass it around</a>`);
+    } else {
+      res.send(`No more redbull! <a href ="/">Please start over</a>`);
+    }
+  });
+  
+
+app.listen(3000, function(){
+    console.log("express is listening for requests from the browser")
+})
